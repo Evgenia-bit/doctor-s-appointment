b0c2b9bc4a13e3ee6238a85a11eec6b1fee61219
Evgenia-bit <ev.mo4alina@yandex.ru>
2022-07-14 09:40:20 +0300

    Variable names in the shedulers have been changed

--- src/shedulers/schedulers.ts
+++ src/shedulers/schedulers.ts
@@ -4,7 +4,7 @@ import { Appointment, AppointmentInput } from '../models/appointment.model'
 import { User, UserInput } from '../models/user.model'
 import { Doctor, DoctorInput } from '../models/doctor.model'
 
-function sendMsg(msg: string):void {
+function sendMessage(msg: string):void {
     fs.appendFile('notices.log', msg, function (err: any) {
         if (err) throw err;
         const fileContent = fs.readFileSync("notices.log", "utf8")
@@ -18,22 +18,22 @@ module.exports = () => {
         const appointments: AppointmentInput[] | null = await Appointment.find()
         let user: UserInput | null
         let doctor: DoctorInput | null
-        let dateSlot: Date
-        let hours: string
-        let messageText: string
+        let slot: Date
+        let hoursBeforeAppointment: string
+        let message: string
         for(const appointment of appointments ) {
-            dateSlot = new Date(appointment.slot)
-            hours = ((dateSlot.getTime() - dateNow.getTime()) / (1000 * 60 * 60)).toFixed(2)
-            if(hours === '24.00') {
+            slot = new Date(appointment.slot)
+            hoursBeforeAppointment = ((slot.getTime() - dateNow.getTime()) / (1000 * 60 * 60)).toFixed(2)
+            if(hoursBeforeAppointment === '24.00') {
                 user = await User.findById(appointment.user_id)
                 doctor = await Doctor.findById(appointment.doctor_id)
-                messageText = `${dateNow} | Привет, ${user?.name}! Напоминаем что вы записаны к ${doctor?.spec}у завтра в ${dateSlot}!`
-                sendMsg(messageText)
-            } else if(hours === '2.00') {
+                message = `${dateNow} | Привет, ${user?.name}! Напоминаем что вы записаны к ${doctor?.spec}у завтра в ${slot}!`
+                sendMessage(message)
+            } else if(hoursBeforeAppointment === '2.00') {
                 user = await User.findById(appointment.user_id)
                 doctor = await Doctor.findById(appointment.doctor_id)
-                messageText = `${dateNow.toLocaleString()} | Привет, ${user?.name}! Вам через 2 часа к ${doctor?.spec}у в ${dateSlot.toLocaleString()}!`
-                sendMsg(messageText)
+                message = `${dateNow.toLocaleString()} | Привет, ${user?.name}! Вам через 2 часа к ${doctor?.spec}у в ${slot.toLocaleString()}!`
+                sendMessage(message)
             }
         }
     })
