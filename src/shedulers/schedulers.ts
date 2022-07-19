57a22f7b90c762fdafe5928e1255fa60cd831ec0
Evgenia-bit <ev.mo4alina@yandex.ru>
2022-07-19 14:43:03 +0300

    Improved code purity

--- src/shedulers/schedulers.ts
+++ src/shedulers/schedulers.ts
@@ -1,40 +1,102 @@
 const cron = require('node-cron')
 import fs = require('fs')
+
 import { Appointment, AppointmentInput } from '../models/appointment.model'
 import { User, UserInput } from '../models/user.model'
 import { Doctor, DoctorInput } from '../models/doctor.model'
 
-function sendMessage(msg: string):void {
-    fs.appendFile('notices.log', msg, function (err: any) {
-        if (err) throw err;
-        const fileContent = fs.readFileSync("notices.log", "utf8")
-        console.log('Saved!', fileContent)
-    })
+
+class Notification {
+
+    dateNow: Date
+    appointment: AppointmentInput
+
+    constructor(dateNow: Date, appointment: AppointmentInput ) {
+        this.dateNow = dateNow
+        this.appointment = appointment
+    }
+
+    protected async getDataForGenerateMessage() {
+        const user: UserInput | null = await User.findById(this.appointment.user_id)
+        if(!user) {
+            throw new Error( 'Пользователь не найден')
+        }
+
+        const doctor: DoctorInput | null = await Doctor.findById(this.appointment.doctor_id)
+        if(!doctor) {
+            throw new Error( 'Доктор не найден')
+        }
+
+        return {
+            userName: user.name,
+            doctorSpec: doctor.spec,
+            slot: this.appointment.slot
+        }
+    }
+
+    sendMessage(message: string): void {
+        fs.appendFile('notices.log', message, function (err: any) {
+            if (err) throw err
+            const fileContent = fs.readFileSync("notices.log", "utf8")
+            console.log('Saved!', fileContent)
+        })
+    }
+}
+
+class TwentyFourHoursNotification extends Notification {
+    async generateMessage(): Promise<string> {
+        const { userName, doctorSpec, slot } = await this.getDataForGenerateMessage()
+        return `${this.dateNow.toLocaleString()} | Привет, ${userName}! Напоминаем что вы записаны к ${doctorSpec}у завтра в ${slot.toLocaleString()}!`
+    }
+}
+
+class TwoHoursNotification extends Notification {
+    async generateMessage(): Promise<string> {
+        const { userName, doctorSpec, slot } = await this.getDataForGenerateMessage()
+        return  `${this.dateNow.toLocaleString()} | Привет, ${userName}! Вам через 2 часа к ${doctorSpec}у в ${slot.toLocaleString()}!`
+    }
+}
+
+async function createNotification(notification: TwoHoursNotification | TwentyFourHoursNotification): Promise<void> {
+    const message = await notification.generateMessage()
+    notification.sendMessage(message)
+}
+
+function getHoursBeforeAppointment(slot: Date, dateNow: Date): string {
+    return ((slot.getTime() - dateNow.getTime()) / (1000 * 60 * 60)).toFixed(2)
+}
+
+function isTwentyFourHoursLeft(hours: string): boolean {
+    return hours === '24.00'
+}
+
+function isTwoHoursLeft(hours: string): boolean  {
+    return hours === '2.00'
 }
 
 module.exports = () => {
     cron.schedule('* * * * *', async () => {
-        const dateNow: Date = new Date()
-        const appointments: AppointmentInput[] | null = await Appointment.find()
-        let user: UserInput | null
-        let doctor: DoctorInput | null
-        let slot: Date
-        let hoursBeforeAppointment: string
-        let message: string
-        for(const appointment of appointments ) {
-            slot = new Date(appointment.slot)
-            hoursBeforeAppointment = ((slot.getTime() - dateNow.getTime()) / (1000 * 60 * 60)).toFixed(2)
-            if(hoursBeforeAppointment === '24.00') {
-                user = await User.findById(appointment.user_id)
-                doctor = await Doctor.findById(appointment.doctor_id)
-                message = `${dateNow} | Привет, ${user?.name}! Напоминаем что вы записаны к ${doctor?.spec}у завтра в ${slot}!`
-                sendMessage(message)
-            } else if(hoursBeforeAppointment === '2.00') {
-                user = await User.findById(appointment.user_id)
-                doctor = await Doctor.findById(appointment.doctor_id)
-                message = `${dateNow.toLocaleString()} | Привет, ${user?.name}! Вам через 2 часа к ${doctor?.spec}у в ${slot.toLocaleString()}!`
-                sendMessage(message)
+        try {
+            const dateNow: Date = new Date()
+            const appointments: AppointmentInput[] | null = await Appointment.find()
+            let hoursBeforeAppointment: string
+            let notification: TwentyFourHoursNotification | TwoHoursNotification
+
+            for(const appointment of appointments ) {
+                hoursBeforeAppointment = getHoursBeforeAppointment(new Date(appointment.slot), dateNow)
+
+                if(isTwentyFourHoursLeft(hoursBeforeAppointment)) {
+                    notification = new TwentyFourHoursNotification(dateNow, appointment)
+                    await createNotification(notification)
+                }
+                else if(isTwoHoursLeft(hoursBeforeAppointment)) {
+                    notification = new TwoHoursNotification(dateNow, appointment)
+                    await createNotification(notification)
+                }
             }
+        } catch (e: any) {
+            console.error(e.message)
         }
     })
 }
+
