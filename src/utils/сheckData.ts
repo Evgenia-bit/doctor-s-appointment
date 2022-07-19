b17c06ae5e0fb2621aa4a229dde1ee0451d333a6
Evgenia-bit <ev.mo4alina@yandex.ru>
2022-07-19 14:46:47 +0300

    Added return true

--- src/utils/сheckData.ts
+++ src/utils/сheckData.ts
@@ -8,32 +8,38 @@ class СheckData {
         if (!Types.ObjectId.isValid(id)) {
             throw new ApiError( 'ID не валиден', 400)
         }
+        return true
     }
     static bodyIsExisting (body: any) {
         if (!Object.keys(body).length) {
             throw new ApiError( 'Поля обязательны для заполнения', 400)
         }
+        return true
     }
     static slotIsExisting (doctor: DoctorInput, slot: string) {
         if (!doctor.slots.includes(slot)) {
             throw new ApiError( 'Данного слота не существует', 404)
         }
+        return true
     }
     static slotIsNotExisting (doctor: DoctorInput, slot: string) {
         if (doctor.slots.includes(slot)) {
             throw new ApiError( 'Данный слот уже существует', 409)
         }
+        return true
     }
     static slotIsValid (slot: string) {
         if (slot.length !== 19 &&  isNaN(Date.parse(slot))) {
             throw new ApiError( 'Слот введён в неверном формате. Все даты должны быть записаны в формате YYYY-MM-DDThh:mm:ss', 400)
         }
+        return true
     }
     static async slotIsNotOccupied (slot: string) {
         const appointmentForThisSlot: AppointmentInput | null = await Appointment.findOne({slot})
         if (appointmentForThisSlot) {
             throw new ApiError( 'Данный слот уже занят', 409)
         }
+        return true
     }
 }
 
